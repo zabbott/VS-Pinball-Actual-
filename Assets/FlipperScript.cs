91f6652b34d77d2a594b8b6ed16adefe9d9fca57
zabbott <zabbott@mail.bradley.edu>
2017-01-22 13:37:45 -0600

    Test

---
+++ Assets/FlipperScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperScript : MonoBehaviour {
+
+
+
+	public HingeJoint2D LeftFlipperHinge; 
+	public JointMotor2D LeftFlipperHingeMotor; 
+
+	// Use this for initialization
+	void Start () {
+		LeftFlipperHinge = gameObject.GetComponent<HingeJoint2D> ();
+		LeftFlipperHingeMotor = LeftFlipperHinge.motor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) { 
+			print ("A is being pressed");
+			LeftFlipperHinge.useMotor = true;
+
+		} 
+
+		if (gameObject.transform.rotation.z > 0) { 
+
+			LeftFlipperHinge.useMotor = false;
+
+		} 
+
+	}
+
+
+}
---
+++ Assets/RightFlipperScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightFlipperScript : MonoBehaviour {
+
+
+
+	public HingeJoint2D LeftFlipperHinge; 
+	public JointMotor2D LeftFlipperHingeMotor; 
+
+	// Use this for initialization
+	void Start () {
+		LeftFlipperHinge = gameObject.GetComponent<HingeJoint2D> ();
+		LeftFlipperHingeMotor = LeftFlipperHinge.motor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.D)) { 
+			print ("D is being pressed");
+			LeftFlipperHinge.useMotor = true;
+
+		} 
+
+		if (gameObject.transform.rotation.z < 0) { 
+
+			LeftFlipperHinge.useMotor = false;
+
+		} 
+
+	}
+
+
+}
